9508f9dc69a6cab7328b4613d04984d9ff35db90
Eloise Barrow <eloise@chartbeat.com>
2020-06-20 17:04:47 -0400

    update resolve functions to query models on db, remove dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -12,22 +12,6 @@ const {
     GraphQLList
 } = graphql;
 
-// dummy data
-// var books = [
-//     { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
-//     { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
-//     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
-//     { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
-//     { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
-//     { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' }
-// ];
-
-// var authors = [
-//     { name: 'Patrick Rothfuss', age: 44, id: '1' },
-//     { name: 'Brandon Sanderson', age: 42, id: '2' },
-//     { name: 'Terry Pratchett', age: 66, id: '3' }
-// ]
-
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({ // fields must be wrapped in a function otherwise JS will not know what AuthorType is (and vice versa if order of code is switched)
@@ -38,7 +22,7 @@ const BookType = new GraphQLObjectType({
             type: AuthorType, // single AuthorType associated with each book
             resolve(parent, args){
                 console.log(parent);
-                // return _.find(authors, { id: parent.authorId })
+                return Author.findById(parent.authorId);
             }
         }
     })
@@ -53,7 +37,7 @@ const AuthorType = new GraphQLObjectType({
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args){
-                // return _.filter(books, {authorId: parent.id })
+                return Book.find({ authorId: parent.id });
             }
         }
     })
@@ -66,27 +50,26 @@ const RootQuery = new GraphQLObjectType({ // RootQuery: how we initially jump in
             type: BookType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args){ // this function fires when 'book' is seen in a query
-                // code to get data from db / other source
-                // return _.find(books, { id: args.id }); //  use lodash to look thru books array and find id of args.id
+                return Book.findById(args.id);
             }
         },
         author: { // single author
             type: AuthorType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args){
-                // return _.find(authors, { id: args.id });
+                return Author.findById(args.id);
             }
         },
         books: { // all books
             type: new GraphQLList(BookType),
             resolve(parent, args){
-                // return books;
+                return Book.find({});
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args){
-                // return authors;
+                return Author.find({});
             }
         }
     }
