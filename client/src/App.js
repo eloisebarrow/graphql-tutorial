bb054b25f8502121c7f8760768cac727a690ff2c
Eloise Barrow <eloise@chartbeat.com>
2020-06-20 17:49:29 -0400

    install apollo-boost, apollo-react, wrap App in ApolloClient, define endpoint for Apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
 // components
 import BookList from './components/BookList';
 
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql' // get data from this endpoint
+});
+
 function App() {
   return (
-    <div id="main">
+    <ApolloProvider client={client}> 
+      <div id="main">
       <h1>GraphQL Tutorial</h1>
       <BookList />
-    </div>
-  );
+      </div>
+    </ApolloProvider>
+  )
 }
 
 export default App;
