28e5c72f6e73dcf71f93128a303b205d5f9b157b
Eloise Barrow <eloise@chartbeat.com>
2020-06-20 17:27:15 -0400

    add BookList component and render it from App

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// components
+import BookList from './components/BookList';
+
 function App() {
   return (
     <div id="main">
       <h1>GraphQL Tutorial</h1>
+      <BookList />
     </div>
   );
 }
