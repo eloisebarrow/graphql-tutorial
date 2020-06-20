7c19e5de104c1db836ddd9dff1c5c943f6156c18
Eloise Barrow <eloise@chartbeat.com>
2020-06-20 16:10:34 -0400

    install mongoose, connect mongoose to mongodb instance

--- server/app.js
+++ server/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql'); // provides simple way to create express server that runs graphql API by using middleware on a single route
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(
+    'mongodb+srv://eloise:test123@graphql-tutorial-bnc1c.mongodb.net/test', 
+    { useNewUrlParser: true, // this object added due to mongoose Server Discovery and Monitoring engine deprecation warning
+      useUnifiedTopology: true
+    } 
+); 
+
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({ // middleware
     schema,
